2631684772253d0163bb1c04d228a8bdddabe51b
Jack Polgar <nrx@nirix.net>
2012-06-13 02:37:39 +1000

    Use LGPL

--- lib/rocketeer/version.rb
+++ lib/rocketeer/version.rb
@@ -2,10 +2,19 @@
 # Rocketeer
 # Copyright (C) 2011-2012 Jack Polgar
 # All Rights Reserved
-# http://github.com/nirix/rockets
+# http://github.com/nirix/rocketeer
 #
-# Relased under the BSD 3-clause / New BSD License
-# Please see the COPYING file for details.
+# Rocketeer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; version 3 only.
+#
+# Rocketeer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Rocketeer. If not, see <http://www.gnu.org/licenses/>.
 #
 
 module Rocketeer
