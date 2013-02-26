4fed981fdf32a768d20b407fb7eb281d5f0d2f3a
Jack Polgar <nrx@nirix.net>
2013-02-26 19:36:30 +1100

    Added `link_to_unless_current` test.

--- tests/html.rb
+++ tests/html.rb
@@ -18,6 +18,16 @@ get '/html/link_to' do
 end
 
 #########################################
+# link_to_unless_current
+get '/html/link_to_unless_current' do
+  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current'
+end
+
+get '/html/link_to_unless_current/2' do
+  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current', class: 'test'
+end
+
+#########################################
 # Tests
 
 describe 'HTML Helpers' do
@@ -31,5 +41,16 @@ describe 'HTML Helpers' do
   it "link_to" do
     get '/html/link_to'
     last_response.body.should.equal '<a class="btn-delete" href="/users/1/delete">Delete User</a>'
+  end
+
+  # link_to_unless_current
+  it "link_to_unless_current" do
+    get '/html/link_to_unless_current'
+    current = last_response.body.should.equal('link_to_unless_current')
+
+    get '/html/link_to_unless_current/2'
+    not_current = last_response.body.should.equal('<a class="test" href="/html/link_to_unless_current">link_to_unless_current</a>')
+
+    current and not_current
   end
 end
