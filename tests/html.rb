926fa6a7e09d33a00709c4199250adbd03089d7e
Jack Polgar <nrx@nirix.net>
2013-02-26 19:43:57 +1100

    Split the `link_to_unless_current` test into two tests.

--- tests/html.rb
+++ tests/html.rb
@@ -20,11 +20,12 @@ end
 #########################################
 # link_to_unless_current
 get '/html/link_to_unless_current' do
-  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current'
+  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current/2', class: 'test'
 end
 
 get '/html/link_to_unless_current/2' do
-  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current', class: 'test'
+  link_to_unless_current 'link_to_unless_current', '/html/link_to_unless_current/2', class: 'test'
+end
 end
 
 #########################################
@@ -44,13 +45,15 @@ describe 'HTML Helpers' do
   end
 
   # link_to_unless_current
-  it "link_to_unless_current" do
+  it "link_to_unless_current - link" do
     get '/html/link_to_unless_current'
-    current = last_response.body.should.equal('link_to_unless_current')
+    last_response.body.should.equal('<a class="test" href="/html/link_to_unless_current/2">link_to_unless_current</a>')
+  end
 
+  it "link_to_unless_current - no link" do
     get '/html/link_to_unless_current/2'
-    not_current = last_response.body.should.equal('<a class="test" href="/html/link_to_unless_current">link_to_unless_current</a>')
+    last_response.body.should.equal('link_to_unless_current')
+  end
 
-    current and not_current
   end
 end
